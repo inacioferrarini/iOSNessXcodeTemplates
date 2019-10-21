5191970949fcb0e57aab1b10db6ccce9d368ca33
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-10-21 19:52:36 -0300

    Adds template for Details ViewController

---
+++ Ness/Details ViewController.xctemplate/___FILEBASENAME___DetailsView.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+protocol ___VARIABLE_productName:identifier___DetailsViewDelegate: AnyObject {
+    // func ___VARIABLE_variableName:identifier___DetailsView(_ simpleView: ___VARIABLE_productName:identifier___DetailsView, didSelect value: Any)
+    // func ___VARIABLE_variableName:identifier___DetailsViewDidSelectedOption(_ simpleView: ___VARIABLE_productName:identifier___DetailsView)
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak private(set) public var contentView: UIView!
+    @IBOutlet weak private(set) var descriptionLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    weak var delegate: ___VARIABLE_productName:identifier___DetailsViewDelegate?
+
+    var ___VARIABLE_modelVariable:identifier___: ___VARIABLE_modelClass:identifier___? {
+        didSet {
+            setup(with: ___VARIABLE_modelVariable:identifier___)
+        }
+    }
+
+    // MARK: - Initialization
+    
+    ///
+    /// Initializes the view with using `UIScreen.main.bounds` as frame.
+    ///
+    public required init() {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    ///
+    /// Initializes the view with using the given `frame`.
+    /// - Parameter frame: Initial view dimensions.
+    ///
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    ///
+    /// Initializes the view with using the given `coder`.
+    /// - Parameter aDecoder: NSCoder to be used.
+    ///
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: type(of: self))
+        let className = String(describing: type(of: self))
+        bundle.loadNibNamed(className, owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        setupView()
+    }
+    
+    ///
+    /// Override this method in order to provide aditional view setup.
+    /// This method will be called after the initialization.
+    ///
+    open func setupView() {}
+
+    private func setup(with ___VARIABLE_modelVariable:identifier___: ___VARIABLE_modelClass:identifier___?) {
+        guard let ___VARIABLE_modelVariable:identifier___ = ___VARIABLE_modelVariable:identifier___ else { return }
+        // descriptionLabel.text = ""
+    }
+   
+}
---
+++ Ness/Details ViewController.xctemplate/___FILEBASENAME___DetailsViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+protocol ___VARIABLE_productName:identifier___DetailsViewControllerDelegate: AnyObject {
+    // func ___VARIABLE_variableName:identifier___DetailsViewController(_ ___VARIABLE_variableName:identifier___DetailsViewController: ___VARIABLE_productName:identifier___DetailsViewController, didSelect value: Any)
+    // func ___VARIABLE_variableName:identifier___DetailsViewControllerDidSelectedOption(_ ___VARIABLE_variableName:identifier___DetailsViewController: ___VARIABLE_productName:identifier___DetailsViewController)
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak private(set) var ___VARIABLE_variableName:identifier___DetailsView: ___VARIABLE_productName:identifier___DetailsView!
+    
+    // MARK: - Properties
+    
+    weak var delegate: ___VARIABLE_productName:identifier___DetailsViewControllerDelegate?
+
+    var ___VARIABLE_modelVariable:identifier___: ___VARIABLE_modelClass:identifier___? {
+        didSet {
+            guard let view = ___VARIABLE_variableName:identifier___DetailsView else { return }
+            view.___VARIABLE_modelVariable:identifier___ = ___VARIABLE_modelVariable:identifier___
+        }
+    }
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.___VARIABLE_variableName:identifier___DetailsView.___VARIABLE_modelVariable:identifier___ = self.___VARIABLE_modelVariable:identifier___
+    }
+    
+    private func setup() {
+        self.title = "___VARIABLE_productName:identifier___DetailsViewController"
+        self.___VARIABLE_variableName:identifier___DetailsView.delegate = self
+    }
+
+}
+
+extension ___VARIABLE_productName:identifier___DetailsViewController: ___VARIABLE_productName:identifier___DetailsViewDelegate {
+    
+//    func ___VARIABLE_variableName:identifier___DetailsView(_ ___VARIABLE_variableName:identifier___DetailsView: ___VARIABLE_productName:identifier___DetailsView, didSelect value: Any) {
+//        self.delegate?.___VARIABLE_variableName:identifier___DetailsViewController(self, didSelect: value)
+//    }
+
+//    func ___VARIABLE_variableName:identifier___DetailsViewDidSelectedOption(_ ___VARIABLE_variableName:identifier___DetailsView: ___VARIABLE_productName:identifier___DetailsView) {
+//        self.delegate?.___VARIABLE_variableName:identifier___DetailsViewControllerDidSelectedOption(self)
+//    }
+
+}
